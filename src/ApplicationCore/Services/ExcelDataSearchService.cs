3147bcf66ed7ee5cbd528eee018bb96fc6651f37
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-06-27 18:32:17 +0900

    Create IExcelDataSearchService

---
+++ src/ApplicationCore/Services/ExcelDataSearchService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using DataFormer.ApplicationCore.Interfaces;
+using DataFormer.ApplicationCore.ValueObjects;
+using NPOI.SS.UserModel;
+
+namespace DataFormer.ApplicationCore.Services
+{
+    public class ExcelDataSearchService : IExcelDataSearchService
+    {
+        /// <summary>
+        /// Initializes a new instance of ExcelDataSearchService class.
+        /// </summary>
+        public ExcelDataSearchService()
+        {
+        }
+
+        /// <inheritdoc/>
+        public IList<DateTime?> SearchDateTimes(ISheet sheet, SearchRule rule)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public IList<string?> SearchLabels(ISheet sheet, SearchRule rule)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public IList<double?> SearchNumerics(ISheet sheet, SearchRule rule)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
