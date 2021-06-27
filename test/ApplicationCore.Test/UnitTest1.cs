b71307376ae6654effbcb1bf44b24780e2479a1f
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-06-27 15:14:22 +0900

    Create ApplicationCore.Test project

---
+++ test/ApplicationCore.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DataFormer.ApplicationCore.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
