a098fd67279a6e79e9134351022602ef12dd18e6
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-06-27 15:10:28 +0900

    Create ConsoleApp project

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataFormer.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
