c7ff6b930743229946509c5939f14ac5b30f7126
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-06-27 15:27:17 +0900

    Inpelment ApplicationLogic class

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Threading.Tasks;
+using ConsoleAppFramework;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace DataFormer.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await Host.CreateDefaultBuilder()
+                .ConfigureLogging(logging =>
+                {
+
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    // Dependency injection setting
+                })
+                .RunConsoleAppFrameworkAsync<ApplicationLogic>(args);
+        }
+    }
+
+    public class ApplicationLogic : ConsoleAppBase
+    {
+        private readonly ILogger<ApplicationLogic> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of ApplicationLogic class.
+        /// </summary>
+        /// <param name="logger">logger object</param>
+        public ApplicationLogic(
+            ILogger<ApplicationLogic> logger
+        )
+        {
+            _logger = logger;
+        }
+
+        [Command("xlsx", "extract data from excel file")]
+        public void ExtractDataFromExcelFile(
+            [Option("i", "input Excel file path.")] string inputFilePath,
+            [Option("o", "output Excel file path.")] string outputFilePath
+        )
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
