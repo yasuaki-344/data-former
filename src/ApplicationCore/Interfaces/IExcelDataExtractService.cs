0b0025b2288eabd2d3d17b8a7e5af8a145215689
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-06-27 15:58:20 +0900

    Create IExcelDataExtractService interface

---
+++ src/ApplicationCore/Interfaces/IExcelDataExtractService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NPOI.SS.UserModel;
+
+namespace DataFormer.ApplicationCore.Interfaces
+{
+    public interface IExcelDataExtractService
+    {
+        DateTime? ReadDateTime(ISheet sheet, int rowIndex, int columnIndex);
+        string? ReadLabel(ISheet sheet, int rowIndex, int columnIndex);
+        double? ReadValue(ISheet sheet, int rowIndex, int columnIndex);
+    }
+}
---
+++ src/ApplicationCore/Services/ExcelDataExtractService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DataFormer.ApplicationCore.Interfaces;
+using NPOI.SS.UserModel;
+
+namespace DataFormer.ApplicationCore.Services
+{
+    public class ExcelDataExtractService : IExcelDataExtractService
+    {
+        /// <summary>
+        /// Initializes a new instance of ExcelDataExtractService class.
+        /// </summary>
+        public ExcelDataExtractService()
+        {
+        }
+
+        public DateTime? ReadDateTime(ISheet sheet, int rowIndex, int columnIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? ReadLabel(ISheet sheet, int rowIndex, int columnIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double? ReadValue(ISheet sheet, int rowIndex, int columnIndex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
