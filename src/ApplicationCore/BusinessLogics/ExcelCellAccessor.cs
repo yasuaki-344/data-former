a1dae74ddbd478f86db2cadfbc6df67e589e8ba9
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-07-04 14:12:30 +0900

    Create ExcelCellAccessor
    Create IExcellCellAccessor interface and ExcelCellAccessor class
    to provide the methods to get excel cell object.

---
+++ src/ApplicationCore/BusinessLogics/ExcelCellAccessor.cs
@@ -0,0 +1,31 @@
+﻿using DataFormer.ApplicationCore.Interfaces;
+using NPOI.SS.UserModel;
+
+namespace DataFormer.ApplicationCore.BusinessLogics
+{
+    public class ExcelCellAccessor : IExcelCellAccessor
+    {
+        /// <summary>
+        /// Initializes a new instance of ExcelCellAccessor class.
+        /// </summary>
+        public ExcelCellAccessor()
+        {
+        }
+
+        /// <inheritdoc/>
+        public ICell? GetReadCell(ISheet sheet, int rowIndex, int columnIndex)
+        {
+            var row = sheet.GetRow(rowIndex);
+            var cell = row?.GetCell(columnIndex);
+            return cell;
+        }
+
+        /// <inheritdoc/>
+        public ICell GetWriteCell(ISheet sheet, int rowIndex, int columnIndex)
+        {
+            var row = sheet.GetRow(rowIndex) ?? sheet.CreateRow(rowIndex);
+            var cell = row.GetCell(columnIndex) ?? row.CreateCell(columnIndex);
+            return cell;
+        }
+    }
+}
---
+++ src/ApplicationCore/Interfaces/IExcellCellAccessor.cs
@@ -0,0 +1,25 @@
+﻿using NPOI.SS.UserModel;
+
+namespace DataFormer.ApplicationCore.Interfaces
+{
+    public interface IExcelCellAccessor
+    {
+        /// <summary>
+        /// Gets the cell of specified row and column index in the specified sheet
+        /// </summary>
+        /// <param name="sheet">Excel sheet object</param>
+        /// <param name="rowIndex">Row index of cell</param>
+        /// <param name="columnIndex">Column index of cell</param>
+        /// <returns></returns>
+        ICell? GetReadCell(ISheet sheet, int rowIndex, int columnIndex);
+
+        /// <summary>
+        /// Gets or creates the cell of specified row and column index in the specified sheet
+        /// </summary>
+        /// <param name="sheet">Excel sheet object</param>
+        /// <param name="rowIndex">Row index of cell</param>
+        /// <param name="columnIndex">Column index of cell</param>
+        /// <returns></returns>
+        ICell GetWriteCell(ISheet sheet, int rowIndex, int columnIndex);
+    }
+}
