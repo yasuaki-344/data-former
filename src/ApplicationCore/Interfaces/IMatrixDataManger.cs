74d601fbedfe867246568bc96a287535f96b653c
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-07-04 14:14:23 +0900

    Create MatrixDataManager
    Create IMatrixDataManager interface and MatrixDataManager class
    to provide the methods to get cell position.

---
+++ src/ApplicationCore/Interfaces/IMatrixDataManger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using DataFormer.ApplicationCore.Entities;
+
+namespace DataFormer.ApplicationCore.Interfaces
+{
+    public interface IMatrixDataManger
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="block"></param>
+        /// <returns></returns>
+        int GetMaxDataNumber(List<SearchConfig> block);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="Row"></param>
+        /// <param name="index"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        (int Row, int Column) GetPosition(int index, SearchConfig config);
+    }
+}
---
+++ test/ApplicationCore.Test/MatrixDataMangerTest.cs
@@ -0,0 +1,47 @@
+﻿using DataFormer.ApplicationCore.BusinessLogics;
+using DataFormer.ApplicationCore.Entities;
+using DataFormer.ApplicationCore.ValueObjects;
+using Xunit;
+
+namespace DataFormer.ApplicationCore.Test
+{
+    public class MatrixDataMangerTest
+    {
+        [Fact]
+        public void GetPositionCorrectly()
+        {
+            var target = new MatrixDataManger();
+
+            var rule = new SearchConfig
+            {
+                Direction = SearchDirection.Row,
+                InitialRowPostion = 0,
+                InitialColumnPosition = 0,
+                RowSize = 3,
+                ColumnSize = 3,
+                RowIncrement = 1,
+                ColumnIncrement = 1,
+            };
+
+            var actual = target.GetPosition(0, rule);
+            Assert.Equal(0, actual.Row);
+            Assert.Equal(0, actual.Column);
+
+            actual = target.GetPosition(3, rule);
+            Assert.Equal(0, actual.Row);
+            Assert.Equal(1, actual.Column);
+
+            actual = target.GetPosition(4, rule);
+            Assert.Equal(1, actual.Row);
+            Assert.Equal(1, actual.Column);
+
+            actual = target.GetPosition(7, rule);
+            Assert.Equal(1, actual.Row);
+            Assert.Equal(2, actual.Column);
+
+            actual = target.GetPosition(8, rule);
+            Assert.Equal(2, actual.Row);
+            Assert.Equal(2, actual.Column);
+        }
+    }
+}
